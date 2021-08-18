5f83eb5d71547aed97d4722ddd8a659c5083551d
PuriPuriPurin <PuriPuriPurin@users.noreply.github.com>
2021-08-18 13:51:51 +0000

    deploy: e3215d9faed333e3011c02edc7937b53f6d07338

--- _next/static/IPeXqX47wxb6TPj7Wgi67/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/56-44d2ee5c87c00dffa439.js","static/chunks/pages/index-da7bf13311ddaff99b56.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
