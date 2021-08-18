9a2165ff0b1485c08d79e553fa6d7abfacd10928
PuriPurin <waket26@gmail.com>
2021-08-18 22:16:55 +0900

    OGP 対応

--- src/components/commonHead.tsx
+++ src/components/commonHead.tsx
@@ -1,20 +1,37 @@
 import Head from 'next/head';
 
-const CommonHead = (): JSX.Element => (
-  <Head>
-    <title key='title'>Todo List</title>
-    {/* レスポンシブ対応（できてるの？） */}
-    <meta name='viewport' content='width=device-width,initial-scale=1.0' />
-    {/* 拡大をできなくする場合 */}
-    {/* <meta name='viewport' content='width=device-width,initial-scale=1.0,user-scalable=no' /> */}
+const CommonHead = (): JSX.Element => {
+  const title = 'Todo List';
+  const url = 'https://puripuripurin.github.io/TodoList/';
+  return (
+    <Head>
+      <title key='title'>{title}</title>
+      {/* レスポンシブ対応（できてるの？） */}
+      <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+      {/* 拡大をできなくする場合 */}
+      {/* <meta
+        name='viewport'
+        content='width=device-width,initial-scale=1.0,user-scalable=no'
+      /> */}
 
-    {/* 文字コード指定（日本語サイトで必須） */}
-    <meta charSet='utf-8' />
-    {/* favicon の設定 */}
-    {/* <link rel='icon' href='/favicon.ico' /> */}
-    {/* 検索結果に表示される説明文 */}
-    {/* <meta name='description' key='description' content='説明。' /> */}
-  </Head>
-);
+      {/* 文字コード指定（日本語サイトで必須） */}
+      <meta charSet='utf-8' />
+      {/* favicon の設定 */}
+      {/* <link rel='icon' href='/favicon.ico' /> */}
+      {/* 検索結果に表示される説明文 */}
+      {/* <meta name='description' key='description' content='説明。' /> */}
+
+      <meta name='description' content='プログラム初学者がよく作りがちなよくあるTodoリストです。' />
+      <meta property='og:url' content={url} />
+      <meta property='og:title' content={title} />
+      <meta property='og:site_name' content={title} />
+      <meta name='og:description' content='プログラム初学者がよく作りがちなよくあるTodoリストです。' />
+      <meta name='og:type' content='website' />
+      <meta name='og:image' content='/ogp.png' />
+      <meta name='og:image:width' content='1200' />
+      <meta name='og:image:height' content='630' />
+    </Head>
+  );
+};
 
 export default CommonHead;
